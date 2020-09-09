0f98f5035ef30f668955c27ff8424519f6e3c82f
Oscar Gomes <medinagomes.oscar7@gmail.com>
2020-09-09 19:47:39 -0300

    Update ramdom.c

--- ramdom.c
+++ ramdom.c
@@ -5,24 +5,17 @@
 void main(void) {
     int temaSorteado, i;
 
-    char* nomes[9];
+    char* nomes[4];
 
     nomes[0] = "Dicas de estudo por meio da tecnologia";
-    nomes[1] = "Tenha uma rotina saudavel de acessos";
-    nomes[2] = "Tenha animo por meio da tecnologia";
-    nomes[3] = "Seja produtivo por meio da tecnologia";
-    nomes[4] = "Imoralidade sexual por meio de rede sociais";
-    nomes[5] = "Tenha um trabalho e futuro por meio da tecnologia";
-    nomes[6] = "Estresse por excesso de consumo de tecnologia";
-    nomes[7] = "Nao se sentir morno";
-    nomes[8] = "Tenha uma vida equilibrada consumindo tecnologia";
-    nomes[9] = "Empreenda por meio da tecnologia";
-
+    nomes[1] = "Tenha ânimo e bons hábitos por meio da tecnologia";
+    nomes[2] = "Tenha um trabalho e futuro por meio da tecnologia";
+    nomes[3] = "Estresse por excesso de consumo de tecnologia";
+    
     srand(time(NULL));
 
-    for (i = 0; i < 1; i++) {
-        temaSorteado = rand() % 1;
+    for (i = 0; i < 4; i++) {
+        temaSorteado = rand() % 4;
         printf("O sorteado foi: %s\n", nomes[temaSorteado]);
     }
 }
- 
